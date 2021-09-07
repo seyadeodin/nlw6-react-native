dee202593c9886d57b9c63e37783c7164b2a8ed9
lucsa <seyadeodin@gmail.com>
2021-09-07 16:31:27 -0300

    aula 1

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SingIn } from './src/screens/SignIn' 
+
+const App: React.FC = () => {
+  return(
+    <SingIn/>
+  )
+}
+
+export default App;
---
+++ src/components/ButtonIcon/styles.ts
@@ -0,0 +1,29 @@
+import  { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    
+    width: 'auto',
+    height: 56,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    flex: 1,
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: 'center'
+  },
+  iconWrapper: {
+    width: 56,
+    height: 56,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: theme.colors.line,
+  },
+  
+})
